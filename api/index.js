a7b9f2bf1aa18c16367b93e725df43f92e7209ec
Rohail Altaf <rohailaltaf@gmail.com>
2017-07-26 16:10:13 -0400

    api initial commit

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+let config = {
+  "client_id" : 29,
+  "dev": true,
+  "image_file": "emoji.png",
+  "text_line_1": "HAPPY HOUR 4-8PM",
+  "text_line_2": "$2 SHOT  $3 PBR  $4 WELL",
+  "weather_zip": 11237,
+  "transition_time": 0.05
+};
+
+app.get('/', function (req, res) {
+  res.json(config)
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
