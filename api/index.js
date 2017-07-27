64e9d82fa5de903db14dde7faac0cbfa0704b586
Rohail Altaf <rohailaltaf@gmail.com>
2017-07-27 12:02:10 -0400

    change to port 80

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ let config = {
   "transition_time": 0.05
 };
 
-let ad = mdns.createAdvertisement(mdns.tcp('http'), 4321);
+let ad = mdns.createAdvertisement(mdns.tcp('http'), 80);
 ad.start();
 
 app.get('/', function (req, res) {
