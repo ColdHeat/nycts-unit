e4c4cc527df7a8877862c73e8a7338ab89c15794
Rohail Altaf <rohailaltaf@gmail.com>
2017-07-31 15:13:12 -0400

    change port

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ let config = {
   "transition_time": 0.05
 };
 
-let ad = mdns.createAdvertisement(mdns.tcp('http'), 80);
+let ad = mdns.createAdvertisement(mdns.tcp('http'), 3000);
 ad.start();
 
 app.get('/', function (req, res) {
