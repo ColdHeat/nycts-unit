02269e8f4c784569bc3f513d2f675c4b4af24a59
Rohail Altaf <rohailaltaf@gmail.com>
2017-07-31 15:15:00 -0400

    change text

--- start.py
+++ start.py
@@ -98,8 +98,8 @@ while True:
             ip = str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
             swapDraw.text((2, 0), 'IP: ' + ip , font=font, fill=red)
         else:
-            swapDraw.text((2, 0), 'NYC TRAIN SIGN : '  , font=font, fill=red)
-            swapDraw.text((68, 0), 'legit. realtime.'  , font=font, fill=green)
+            swapDraw.text((2, 0), 'NYC TRAIN SIGN'  , font=font, fill=red)
+            swapDraw.text((68, 0), ' legit. realtime.'  , font=font, fill=green)
             swapDraw.text((2, 16), '@' , font=font, fill=green)
             swapDraw.text((12, 16), 'n y c t r a i n s i g n' , font=font, fill=orange)
         swap.SetImage(swapImage, 0, 0)
