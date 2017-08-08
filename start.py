ae6ab2ae6a7fdeb2022459c5453a20c6b7695128
Angel <a.angel.martinez@gmail.com>
2017-08-08 16:01:11 -0400

    Return displayError on yahoo fail

--- start.py
+++ start.py
@@ -64,9 +64,9 @@ def drawClear():
     draw.rectangle((0, 0, width, height), fill=black)
     b.matrix.SetImage(image, 0, 0)
 
-def displayError(e):
+def displayError():
     drawClear()
-    draw.text((0 + fontXoffset + 3, 0 + topOffset + 0), e, font=font, fill=orange)
+    draw.text((0 + fontXoffset + 3, 0 + topOffset + 0), "something happened", font=font, fill=orange)
     b.matrix.SetImage(image, 0, 0)
     time.sleep(transition_time)
     drawClear()
@@ -144,7 +144,7 @@ while True:
             result = urllib2.urlopen(yql_url)
         except urllib2.URLError as e:
             print 'error'
-            displayError(e)
+            displayError()
         else:
             data = json.loads(result.read())
             weather = data['query']['results']['channel']['item']['condition']['temp']
@@ -239,5 +239,5 @@ while True:
 ##### EXCEPTION SCREEN #####
     except Exception as e:
         logging.exception("message")
-        displayError(e)
+        displayError()
         pass
