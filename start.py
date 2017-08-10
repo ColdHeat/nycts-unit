a6b26dcc74e18fc249900d2d5f9ab5abd6a056c9
Angel <a.angel.martinez@gmail.com>
2017-08-10 13:19:27 -0400

    Add exception to boot screen with dev ip

--- start.py
+++ start.py
@@ -133,7 +133,13 @@ while True:
         swapImage = Image.new('RGB', (width, height))
         swapDraw  = ImageDraw.Draw(swapImage)
         if dev == True:
-            ip = str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+            try:
+                ip = str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+                logger.info('IP screen', extra={'status': 1, 'job': 'ip_screen'})
+            except Exception as e:
+                logger.info('IP screen', extra={'status': 0, 'job': 'ip_screen'}, exc_info=True)
+                ip = '192.168.0.xxx'
+
             swapDraw.text((2, 0), 'IP: ' + ip , font=font, fill=red)
         else:
             swapDraw.text((2, 0), 'NYC TRAIN SIGN'  , font=font, fill=red)
