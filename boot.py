2a3d7a7e6246dd65de881d344158903c4985a700
Angel <a.angel.martinez@gmail.com>
2017-08-14 15:15:21 -0400

    Copy and paste from github

--- boot.py
+++ boot.py
@@ -6,8 +6,6 @@ def config_matrix(b, config, logger):
     swap = b.matrix.CreateFrameCanvas()
     transition_time = int(config["transition_time"])
     b.matrix.brightness = int(config["brightness"])
-    print transition_time
-
     try:
         swap.Clear()
         swapImage = display.Image.new('RGB', (width, height))
@@ -15,12 +13,12 @@ def config_matrix(b, config, logger):
         if dev == True:
             try:
                 ip = str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
-                logger.info('IP screen', extra={'status': 1, 'job': 'boot_screen'})
+                logger.info('IP screen', extra={'status': 1, 'job': 'ip_screen'})
             except Exception as e:
-                logger.info('IP screen', extra={'status': 0, 'job': 'boot_screen'})
+                logger.info('IP screen', extra={'status': 0, 'job': 'ip_screen'}, exc_info=True)
                 ip = '192.168.0.xxx'
 
-            swapDraw.text((2, 0), 'IP: ' + ip , font=display.font, fill=display.red)
+            swapDraw.text((2, 0), 'IP: ' + ip , font=font, fill=red)
         else:
             logger.info('NYC Train Sign', extra={'status': 1, 'job': 'boot_screen'})
             swapDraw.text((2, 0), 'NYC TRAIN SIGN'  , font=display.font, fill=display.red)
