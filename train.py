849433e99946bbefadbfd9d803d67c68de8282d0
Rohail Altaf <rohailaltaf@gmail.com>
2017-08-29 15:51:36 -0400

    time.sleep

--- train.py
+++ train.py
@@ -129,5 +129,6 @@ class train:
 
                     draw.point((constants.width - 9, 6), fill=constants.black)
                     draw.point((constants.width - 9, 22), fill=constants.black)
-
+            self.base.matrix.Clear()
             self.base.matrix.SetImage(image, 0, 0)
+            time.sleep(5)
