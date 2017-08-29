a342bdd50661787752ada89a36d0380d081d1dca
Rohail Altaf <rohailaltaf@gmail.com>
2017-08-29 16:03:05 -0400

    indent error

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ class train:
 
         for dirs,direction in enumerate(self.train_data):
             time.sleep(self.config["settings"]["transition_time"])
- -          self.drawClear()
+            self.drawClear()
             for row in [0, 1]:
 
                 self.data = self.train_data[direction][row]
