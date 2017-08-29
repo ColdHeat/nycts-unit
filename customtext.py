25a86ef4da9184e125cb5df5764b3e71714c5352
Rohail Altaf <rohailaltaf@gmail.com>
2017-08-29 13:42:56 -0400

    wow much typo

--- customtext.py
+++ customtext.py
@@ -17,5 +17,5 @@ class customtext:
         image = Image.new('RGB', (constants.width, constants.height))
         draw  = ImageDraw.Draw(image)
         draw.text((2, 0), self.config["customtext"]["line_1"] , font=constants.font, fill=constants.red)
-        draw.text((2, 16), self.config["customtext"]["line_2"] , font=contstants.font, fill=constants.blue)
+        draw.text((2, 16), self.config["customtext"]["line_2"] , font=constants.font, fill=constants.blue)
         self.base.matrix.SetImage(image, 0, 0)
