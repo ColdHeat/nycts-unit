a79c99a98bbe9a430782f049e8da52b1eeac5072
Angel Martinez <a.angel.martinez@gmail.com>
2017-09-06 18:21:21 -0400

    Refactor logs push to use client config info

--- system/log_upload.py
+++ system/log_upload.py
@@ -1,16 +1,28 @@
 import requests
+import sys
+import json
+import os
 
-url = "https://api.trainsignapi.com/dev-logs/logs/29/upload"
+config_path = '/home/pi'
 
-querystring = {"clientId":"29"}
+with open(config_path + "/nycts-unit/api/config.json") as json_file:
+    config_file = json.load(json_file)
 
-payload = "~/nycts-unit/logs/logs.json"
+client_id = config_file['settings']['client_id']
+
+url = "https://api.trainsignapi.com/dev-logs/logs/" + client_id + "/upload"
+
+querystring = {client_id: client_id}
+
+file_path = config_path + "/nycts-unit/logs/logs.json"
+
+payload = {'log_file': (None, open(file_path,'rb'))}
 
 headers = {
     'content-type': "application/octet-stream",
-    'x-api-key': "5lz8VPkVUL7gcjN5LsZwu1eArX8A3B2m8VeUfXxf"
+    'x-api-key': config_file['settings']['dev_api_key']
     }
 
-response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+response = requests.request("POST", url, files=payload, headers=headers, params=querystring)
 
-print(response.text)
+open(file_path, 'w').close()
