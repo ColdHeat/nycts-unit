6f08bae6dc3511a9a0e3cb3307b648cc3fa41bff
Angel Martinez <a.angel.martinez@gmail.com>
2017-09-11 12:41:21 -0400

    Fix weather module try and exception

--- weather.py
+++ weather.py
@@ -22,25 +22,22 @@ class weather:
     # Periodically get predictions from server ---------------------------
     def thread(self):
         def queryWeatherEndpoint():
-            try:
-                self.config = self.base.config
+            self.config = self.base.config
 
-                url = "https://api.trainsignapi.com/dev-weather/zipCode/11216"
+            url = "https://api.trainsignapi.com/dev-weather/zipCode/11216"
 
-                querystring = {"":""}
+            querystring = {"":""}
 
-                headers = {'x-api-key': '5lz8VPkVUL7gcjN5LsZwu1eArX8A3B2m8VeUfXxf'}
+            headers = {'x-api-key': '5lz8VPkVUL7gcjN5LsZwu1eArX8A3B2m8VeUfXxf'}
 
-                response = requests.request("GET", url, headers=headers, params=querystring)
+            response = requests.request("GET", url, headers=headers, params=querystring)
 
-                data = json.loads(response.text)
-                self.weather["weather"] = str(int(data['data']['temperature']))
-                self.weather["conditions"] = str(data['data']['summary']).upper()
-
-            except Exception as e:
-                logs.logger.info('Weather module', extra={'status': 0, 'job': 'weather_module'}, exc_info=True)
+            data = json.loads(response.text)
+            self.weather["weather"] = str(int(data['data']['temperature']))
+            self.weather["conditions"] = str(data['data']['summary']).upper()
 
             time.sleep(5)
+
         while True:
             try:
                 if self.weather['state'] == 'demo':
@@ -49,12 +46,15 @@ class weather:
                     weatherQuery = threading.Timer(300.0, queryWeatherEndpoint)
                     weatherQuery.start()
 
-        def draw(self):
-            image = Image.new('RGB', (constants.width, constants.height))
-            draw  = ImageDraw.Draw(image)
+            except Exception as e:
+                logs.logger.info('Weather module', extra={'status': 0, 'job': 'weather_module'}, exc_info=True)
 
-            draw.text((2, 0), 'IT IS ' + self.weather["weather"] + ' DEGREES' , font=constants.font, fill=constants.red)
-            draw.text((2, 16), '& ' + self.weather["conditions"] + ' OUTSIDE', font=constants.font, fill=constants.green)
+    def draw(self):
+        image = Image.new('RGB', (constants.width, constants.height))
+        draw  = ImageDraw.Draw(image)
 
-            self.base.matrix.SetImage(image, 0, 0)
-            time.sleep(self.base.getTransitionTime())
+        draw.text((2, 0), 'IT IS ' + self.weather["weather"] + ' DEGREES' , font=constants.font, fill=constants.red)
+        draw.text((2, 16), '& ' + self.weather["conditions"] + ' OUTSIDE', font=constants.font, fill=constants.green)
+
+        self.base.matrix.SetImage(image, 0, 0)
+        time.sleep(self.base.getTransitionTime())
