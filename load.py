2a91c2a430def843a60b22fda72b1304ddf25a49
Angel Martinez <a.angel.martinez@gmail.com>
2017-09-11 17:09:12 -0400

    Remove threading from load.py

--- load.py
+++ load.py
@@ -15,18 +15,13 @@ class load:
     def __init__(self, base):
         self.base = base
         self.config = base.config
-        t = threading.Thread(target=self.thread)
-        t.daemon = True
-        t.start()
 
     def thread(self):
-        while True:
-
-            def draw(self):
-                swapImage = Image.new('RGB', (constants.width, constants.height))
-                swapDraw  = ImageDraw.Draw(swapImage)
-                swapDraw.text((2, 0), 'NYC TRAIN SIGN'  , font=constants.font, fill=constants.red)
-                swapDraw.text((68, 0), ' legit. realtime.'  , font=constants.font, fill=constants.green)
-                swapDraw.text((2, 16), 'Loading...', font=constants.font, fill=constants.red)
-                swap.SetImage(swapImage, 0, 0)
-                swap = b.SwapOnVSync(swap)
+        def draw(self):
+            swapImage = Image.new('RGB', (constants.width, constants.height))
+            swapDraw  = ImageDraw.Draw(swapImage)
+            swapDraw.text((2, 0), 'NYC TRAIN SIGN'  , font=constants.font, fill=constants.red)
+            swapDraw.text((68, 0), ' legit. realtime.'  , font=constants.font, fill=constants.green)
+            swapDraw.text((2, 16), 'Loading...', font=constants.font, fill=constants.red)
+            swap.SetImage(swapImage, 0, 0)
+            swap = b.SwapOnVSync(swap)
