ae32aead39eda8be33b8a62ab61e573a03b188c3
Angel Martinez <a.angel.martinez@gmail.com>
2017-09-13 14:46:22 -0400

    Move logging for system stuff into its own class

---
+++ systemlogs.py
@@ -0,0 +1,42 @@
+import threading
+import time
+import urllib
+import json
+import urllib2
+import Image
+import ImageDraw
+import constants
+import requests
+import logs
+import math
+
+class systemlogs:
+
+    def __init__(self, base):
+        self.base          = base
+        self.config        = base.config
+        t                  = threading.Thread(target=self.thread)
+        t.daemon           = True
+        t.start()
+
+    def systemLog():
+        logs.logger.info('System Diagnostic', extra={'cpu_usage': psutil.cpu_percent(interval=1),
+        'virtual_memory': psutil.virtual_memory()[2], 'swap_memory': psutil.swap_memory()[3],
+        'disk_usage': psutil.disk_usage('/')[3], 'temp': str((int(subprocess.check_output(['cat', '/sys/class/thermal/thermal_zone0/temp']))/1000) * 9/5 + 32) + ' F',
+        })
+        time.sleep(5)
+
+    def internetSpeedLog():
+        speed_data = subprocess.check_output(['speedtest-cli', '--json'])
+        logs.logger.info('Internet Speed', extra={'speed_test': speed_data})
+        time.sleep(1)
+
+    def thread(self):
+        while True:
+            self.config = self.base.config
+
+            self.systemLog()
+
+            self.internetSpeedLog()
+
+            time.sleep(300)
