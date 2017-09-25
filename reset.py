84378cd15922dcc1a3d3f3d6e199e33c48994d99
Rohail Altaf <rohailaltaf@gmail.com>
2017-09-25 15:34:57 -0400

    prevent reset from killing itself

--- reset.py
+++ reset.py
@@ -3,7 +3,7 @@ from signal import pause
 import os
 
 def hello_darkness_my_old_friend():
-    os.system("sudo pkill -f python")
+    os.system("sudo pkill -f start.py")
     os.system("sudo pkill -f node")
     os.system("sudo rm /etc/wpa_supplicant/wpa_supplicant.conf")
     os.system("sudo reboot")
