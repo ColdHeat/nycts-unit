4fc891fe6e297de75ec87c4c953ce9543c33d6bf
Angel Martinez <a.angel.martinez@gmail.com>
2017-09-25 16:59:29 -0400

    Add in setTimeout for wifi reset

--- system/restartWifi.js
+++ system/restartWifi.js
@@ -1,23 +1,15 @@
 const piWifi = require('pi-wifi');
 
-piWifi.detectSupplicant(function(err, iface, configFile) {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Supplicant running in interface', iface, 'using the configuration file', configFile);
-});
-
-
 piWifi.interfaceDown('wlan0', function(err) {
   if (err) {
     return console.error(err.message);
   }
   console.log('Interface dropped succesfully!');
-});
-
-piWifi.interfaceUp('wlan0', function(err) {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Interface raised succesfully!');
+  setTimeout(5000)
+  piWifi.interfaceUp('wlan0', function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Interface raised succesfully!');
+  });
 });
