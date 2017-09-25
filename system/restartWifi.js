0ae66889a0b4527a7283ae262637c2b38fef2135
Angel Martinez <a.angel.martinez@gmail.com>
2017-09-25 17:22:09 -0400

    Rewrite restartWifi to restartInterface instead of turning it off and back on

--- system/restartWifi.js
+++ system/restartWifi.js
@@ -1,18 +1,8 @@
 const piWifi = require('pi-wifi');
 
-piWifi.interfaceDown('wlan0', function(err) {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Interface dropped succesfully!');
-  setTimeout(5000);
-});
-
-
-piWifi.interfaceUp('wlan0', function(err) {
+piWifi.restartInterface('wlan0', function(err) {
   if (err) {
     return console.error(err.message);
   }
   console.log('Interface raised succesfully!');
-  setTimeout(5000);
 });
