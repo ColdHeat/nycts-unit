037967b7308452798ea3bd0ef2e451fbecbf3caf
Rohail Altaf <rohailaltaf@gmail.com>
2017-09-25 15:31:39 -0400

    kill python in reset.py

--- reset.py
+++ reset.py
@@ -3,6 +3,8 @@ from signal import pause
 import os
 
 def hello_darkness_my_old_friend():
+    os.system("sudo pkill -f python")
+    os.system("sudo pkill -f node")
     os.system("sudo rm /etc/wpa_supplicant/wpa_supplicant.conf")
     os.system("sudo reboot")
 
