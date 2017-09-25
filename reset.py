e78b541a268dfa5f8d1341dcbb0fef677e2330e9
Rohail Altaf <rohailaltaf@gmail.com>
2017-09-25 15:37:52 -0400

    kill load.py too

--- reset.py
+++ reset.py
@@ -4,6 +4,7 @@ import os
 
 def hello_darkness_my_old_friend():
     os.system("sudo pkill -f start.py")
+    os.system("sudo pkill -f load.py")
     os.system("sudo pkill -f node")
     os.system("sudo rm /etc/wpa_supplicant/wpa_supplicant.conf")
     os.system("sudo reboot")
