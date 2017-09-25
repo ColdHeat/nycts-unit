a11b727babf4d462aec889bd727c536022a48cdc
Angel Martinez <a.angel.martinez@gmail.com>
2017-09-25 17:01:45 -0400

    Move interfaceUp out of callback function

--- system/restartWifi.js
+++ system/restartWifi.js
@@ -5,11 +5,14 @@ piWifi.interfaceDown('wlan0', function(err) {
     return console.error(err.message);
   }
   console.log('Interface dropped succesfully!');
-  setTimeout(5000)
-  piWifi.interfaceUp('wlan0', function(err) {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Interface raised succesfully!');
-  });
+  setTimeout(5000);
+});
+
+
+piWifi.interfaceUp('wlan0', function(err) {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Interface raised succesfully!');
+  setTimeout(5000);
 });
