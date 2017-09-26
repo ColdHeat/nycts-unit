782761f9ddc8b87f8e2a5a58d47cf95042fbe60c
Rohail Altaf <rohailaltaf@gmail.com>
2017-09-26 15:42:08 -0400

    revert bonjoir

--- api/bonjour.js
+++ api/bonjour.js
@@ -1,19 +1,7 @@
 const mdns = require('mdns');
 let config = require('./config.json');
 
-const createAd = () => {
-  let newAd = mdns.createAdvertisement(mdns.tcp('http-nycts'), 3000, {
-    name: 'NYCTrainSign-' + config["settings"]["name"] + "-" + Date.now()
-  });
-  return newAd;
-}
-let ad = createAd();
+let ad = mdns.createAdvertisement(mdns.tcp('http-nycts'), 3000, {
+  name: 'NYCTrainSign-' + config["settings"]["name"]
+});
 ad.start();
-
-const restart = () => {
-  ad.stop();
-  ad = createAd();
-  ad.start();
-}
-
-setInterval(() => { restart() }, 300000)
