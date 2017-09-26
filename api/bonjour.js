820646bd3c7600f4b1f614ab3d2f838c53d236e6
Rohail Altaf <rohailaltaf@gmail.com>
2017-09-26 15:27:48 -0400

    bonjour restart bug

--- api/bonjour.js
+++ api/bonjour.js
@@ -1,13 +1,18 @@
 const mdns = require('mdns');
 let config = require('./config.json');
 
-let ad = mdns.createAdvertisement(mdns.tcp('http-nycts'), 3000, {
-  name: 'NYCTrainSign-' + config["settings"]["name"] + "-" + Date.now()
-});
+const createAd = () => {
+  let newAd = mdns.createAdvertisement(mdns.tcp('http-nycts'), 3000, {
+    name: 'NYCTrainSign-' + config["settings"]["name"] + "-" + Date.now()
+  });
+  return newAd;
+}
+let ad = createAd();
 ad.start();
 
 const restart = () => {
   ad.stop();
+  ad = createAd();
   ad.start();
 }
 
