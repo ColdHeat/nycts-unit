469ee67f7272f30a19e9e80ea168d74347f17f80
Angel Martinez <a.angel.martinez@gmail.com>
2017-10-05 16:53:24 -0400

    Add test reboot in schedule

--- schedule.py
+++ schedule.py
@@ -2,6 +2,6 @@ import os
 from crontab import CronTab
 
 cron = CronTab(user=True)
-cron_job = cron.new(command="sudo /sbin/ifdown 'wlan0'", user='pi')
-cron_job.minute.every(1)
+cron_job = cron.new(command="sudo reboot now", user='pi')
+cron_job.minute.every(3)
 cron_job.enable()
