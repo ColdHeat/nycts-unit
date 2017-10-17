4e308bd74ae019314a5e8fcad8f3741d761e29c8
Angel Martinez <a.angel.martinez@gmail.com>
2017-10-17 13:36:15 -0400

    Fix missing argument for log upload to S3

--- system/watchdog/log.py
+++ system/watchdog/log.py
@@ -11,35 +11,28 @@ COFNIG_PATH = '/home/pi'
 
 def check_if_offline():
     config = base().config
-
     if config['settings']['state'] == 'online':
         load_log_file(config)
 
 def load_log_file(config):
     log_file = COFNIG_PATH + "/nycts-unit/logs/logs.json"
-
     try:
         with open(log_file, "rb") as f:
             payload = f.read()
     except:
             payload = {}
-
-    upload_log(config, log_file)
+    upload_log(config, log_file, payload)
 
 def upload_log(config, log_file, payload):
     client_id = config['settings']['client_id']
-
     url = "https://api.trainsignapi.com/prod-logs/logs/" + client_id + "/upload"
-
     querystring = {
         client_id: client_id
         }
-
     headers = {
         'content-type': "application/octet-stream",
         'x-api-key': config['settings']['prod_api_key']
         }
-
     requests.request(
         "POST", url, data=payload, headers=headers, params=querystring)
 
