68cb9c02b913920c6b811e27259e69bcbb7bec25
timothy <timwoo@gmail.com>
2017-10-18 11:34:57 -0400

    debug post body lost in ether

--- headless/app/api.js
+++ headless/app/api.js
@@ -34,6 +34,7 @@ module.exports = function(wifi_manager, callback) {
     // Setup static routes to public assets
     app.use(express.static(path.join(__dirname, "public")));
     app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     // Setup HTTP routes for rendering views
     app.get("/", function(request, response) {
@@ -50,6 +51,9 @@ module.exports = function(wifi_manager, callback) {
     });
 
     app.post("/api/enable_wifi", function(request, response) {
+
+        console.log("Req body" + request.body);
+
         var conn_info = {
             wifi_ssid: request.body.wifi_ssid,
             wifi_passcode: request.body.wifi_passcode,
@@ -73,6 +77,8 @@ module.exports = function(wifi_manager, callback) {
             console.log("Wifi Enabled! - Exiting");
             process.exit(0);
         });
+
+        console.log("Req fresh" + req.fresh);
     });
 
     // Listen on our server
