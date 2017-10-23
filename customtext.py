add572a473f4e24321235cbd9819871971d917cc
timothy <timwoo@gmail.com>
2017-10-23 17:48:07 -0400

    ignore UTF chars in custom text module

--- customtext.py
+++ customtext.py
@@ -18,7 +18,9 @@ class customtext:
         self.config = self.base.config
         image = Image.new('RGB', (constants.width, constants.height))
         draw  = ImageDraw.Draw(image)
-        draw.text((2, 0), self.config["customtext"]["line_1"] + " ", font=constants.font, fill=constants.red)
-        draw.text((2, 16), self.config["customtext"]["line_2"] + " " , font=constants.font, fill=constants.blue)
+        draw.text((2, 0), self.config["customtext"]["line_1"].encode('latin-1', 'ignore') + " ", font=constants.font, fill=constants.red)
+        draw.text((2, 16), self.config["customtext"]["line_2"].encode('latin-1', 'ignore') + " " , font=constants.font, fill=constants.blue)
         self.base.matrix.SetImage(image, 0, 0)
         time.sleep(self.base.getTransitionTime())
+
+
