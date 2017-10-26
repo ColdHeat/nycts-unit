3d84817f2dac43179e9709cc3f69892c352ceed9
Angel Martinez <a.angel.martinez@gmail.com>
2017-10-26 15:05:35 -0400

    Set reboot in config instead of file

--- system/watchdog/spy.py
+++ system/watchdog/spy.py
@@ -17,13 +17,21 @@ def check_hostname():
     sign_id = load_config_file()['settings']['sign_id']
 
     if hostname == sign_id:
-        pass
+        check_dataplicity_install()
     else:
         change_hostname(sign_id)
 
 def change_hostname(sign_id):
     os.system("sudo hostnamectl set-hostname " + sign_id)
+    set_reboot_to_true()
     check_dataplicity_install()
+
+def set_reboot_to_true():
+    try:
+        result = urllib2.urlopen(
+            "http://127.0.0.1:3000/setConfig/settings/reboot/true")
+    except urllib2.URLError as e:
+        print 'Something went wrong setting reboot to true...'
 
 def check_dataplicity_install():
     dataplicity_is_installed = os.path.exists("/var/log/dataplicity.log")
