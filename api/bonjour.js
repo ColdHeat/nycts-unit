a7b1237d69416b5e3782b2409de8814128fbc490
Rohail Altaf <rohailaltaf@gmail.com>
2017-10-31 18:59:43 -0400

    remove bonjour

--- api/bonjour.js
+++
@@ -1,7 +0,0 @@
-const mdns = require('mdns');
-let config = require('./config.json');
-
-let ad = mdns.createAdvertisement(mdns.tcp('http-nycts'), 3000, {
-  name: 'NYCTrainSign-' + config["settings"]["name"]
-});
-ad.start();
