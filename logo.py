58d0cf2f9ffd5fcff9fae43e1b8579e5920477ce
Rohail Altaf <rohailaltaf@gmail.com>
2017-10-31 14:15:13 -0400

    Rohail logo stuff (#16)
    * refactor logo to aws
    
    * call method in init
    
    * missing param
    
    * import missing module
    
    * debug
    
    * fix post request
    
    * self.fetchImage
    
    * remove print messages

--- logo.py
+++ logo.py
@@ -7,15 +7,65 @@ import Image
 import ImageDraw
 import constants
 import logs
+import requests
 
 class logo:
 
     def __init__(self, base):
         self.base          = base
         self.config        = base.config
-        self.pic = Image.open("./api/uploads/" + self.config["logo"]["image_file"])
-        self.pic.thumbnail((128,32), Image.ANTIALIAS)
+        self.pic = None
+        self.loadImageFromFile()
+    def loadImageFromFile(self):
+        try:
+            self.pic = Image.open("./api/uploads/0")
+            self.pic.thumbnail((128,32), Image.ANTIALIAS)
+        except Exception, e:
+            logs.logger.info(
+                'Logo module', extra={
+                    'status': 0,
+                    'job': 'logo_module',
+                    'error': str(e)
+                    })
+            self.getImageURL()
+    def fetchImage(self, link):
+        image = urllib.URLopener()
+        try:
+            image.retrieve(link, "./api/uploads/0")
+            self.pic = Image.open("./api/uploads/0")
+            self.pic.thumbnail((128,32), Image.ANTIALIAS)
+        except Exception, e:
+            logs.logger.info(
+                'Logo module', extra={
+                    'status': 0,
+                    'job': 'logo_module',
+                    'error': str(e)
+                    })
+    def getImageURL(self):
+        try:
+            url = 'https://api.trainsignapi.com/dev-get-image/get'
+            payload = {
+                'clientId': self.config['settings']['client_id'],
+                'signId': self.config['settings']['sign_id'],
+                'logoKey': '0'
+            }
+            headers = {
+                'Content-Type': 'application/json',
+                'x-api-key': self.config['settings']['dev_api_key']
+            }
+            response = requests.request(
+                'POST', url, headers=headers, json=payload)
 
+            self.fetchImage(json.loads(response.text)['link'])
+
+        except Exception, e:
+
+            logs.logger.info(
+                'Logo module', extra={
+                    'status': 0,
+                    'job': 'logo_module',
+                    'error': str(e)
+                    })
     def draw(self):
         self.config = self.base.config
         if self.config["logo"]["updated"] == True:
@@ -26,9 +76,7 @@ class logo:
                 error_message = e.reason
                 logs.logger.info('API logo module', extra={'status': 0, 'job': 'api_logo_update', 'error': str(e)})
             else:
-                self.pic = Image.open("./api/uploads/" + self.config["logo"]["image_file"])
-                self.pic = self.pic.convert('RGB')
-                self.pic.thumbnail((128,32), Image.ANTIALIAS)
+                self.getImageURL()
 
         self.base.matrix.SetImage(self.pic.convert('RGB'), 0, 0)
         time.sleep(self.base.getTransitionTime())
