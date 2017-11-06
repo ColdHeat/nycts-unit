c7fbafd07c5ca9be1a94786956677289b8228cb3
Angel Martinez <a.angel.martinez@gmail.com>
2017-11-06 12:14:56 -0500

    Add removal of dataplicity keys when reset

--- reset.py
+++ reset.py
@@ -6,6 +6,8 @@ def hello_darkness_my_old_friend():
     os.system("sudo pkill -f node")
     os.system("sudo rm /etc/wpa_supplicant/wpa_supplicant.conf")
     os.system("sudo hostnamectl set-hostname nycts")
+    os.system("sudo rm /opt/dataplicity/tuxtunnel/auth")
+    os.system("sudo rm /opt/dataplicity/tuxtunnel/serial")
     os.system("sudo reboot & sudo pkill -f python")
 
 button = Button(25)
