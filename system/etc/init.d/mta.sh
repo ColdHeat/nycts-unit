60eeec98f5f7a7d1101cc632f91c1622415ba329
Angel Martinez <a.angel.martinez@gmail.com>
2017-11-06 15:43:42 -0500

    Move spy to get called with job

--- system/etc/init.d/mta.sh
+++ system/etc/init.d/mta.sh
@@ -7,17 +7,16 @@ sudo python load.py &
 cd /home/pi/nycts-unit/headless
 sudo npm run start
 
-
 cd /home/pi/nycts-unit/api
 node index.js &
-sleep 5
+sleep 10
 pkill -f load.py
 pkill -f readytopair.py
-sudo python /home/pi/nycts-unit/system/watchdog/spy.py
 
 sleep 10
 
-cd /home/pi/nycts-unit/system/watchdog
+cd /home/pi/nycts-unit/
+sudo python system/watchdog/spy.py
 sudo python jobs.py
 
 cd /home/pi/nycts-unit
