723dca6e33dbec17ac0b5b41705d1c79c4c477e5
Rohail Altaf <rohailaltaf@gmail.com>
2017-11-07 22:23:35 -0500

    sleep

--- transit/bart.py
+++ transit/bart.py
@@ -44,5 +44,5 @@ def draw(swap, direction, constants, config, train_data, train_directions, matri
                 draw.text((fontXoffset, fontYoffset + 7), data['length'] + ' CAR TRAIN', font=constants.font, fill=constants.orange)
 
             matrix.SetImage(image, 0, 0)
-        if destination != (len(train_data[direction])-1):
-            time.sleep(int(config['settings']['transition_time']))
+            if destination != (len(train_data[direction])-1):
+                time.sleep(int(config['settings']['transition_time']))
